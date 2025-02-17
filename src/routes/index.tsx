06c97dd23b9cbed83ece5bc10b37fce6ece64260
matheus-rodrigues <matheusolvr.dev@gmail.com>
2025-02-17 16:29:03 -0300

    add: Home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,13 @@
-export { };
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { HomePage } from '../shared/components/home-page';
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path='/home' element={<HomePage />} />
+            
+            // Redirect to home page if no route matches
+            <Route path='*' element={<Navigate to='/home' />} /> 
+        </Routes>
+    );
+};
